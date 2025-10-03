d7155a9c9206e71c2227cb2b90ef974a118a4143
TIFERES DAVID <48111030@ort>
2025-10-03 08:39:49 -0300

    pantalla 4 editada

--- react-app/src/components/Pantalla4.jsx
+++ react-app/src/components/Pantalla4.jsx
@@ -3,7 +3,10 @@ import './Pantalla4.css';
 const Pantalla4 = () => {
   return (
     <section className="pantalla pantalla4">
-      <h1 className="titulo4">ENTRA A NUESTRA WEB Y COMIENZA YA</h1>
+    <div className="Titulillo"><h1 className="titulo4">SEGUIS CON DUDAS? </h1>
+      <h3 className="titulo4 tb"> TE OFRECEMOS ALGO</h3></div>
+        <p className="textitulio">1 MES DE FISA PREMIUM GRATIS SI TE REGISTRAS AHORA</p>
+
       <div className="contenedor-web">
         <img src="/computadora.png" alt="Computadora" className="computadora-img" />
         <div className="info-web">
